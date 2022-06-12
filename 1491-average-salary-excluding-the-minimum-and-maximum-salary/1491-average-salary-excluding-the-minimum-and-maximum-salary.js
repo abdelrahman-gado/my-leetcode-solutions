b6a20dfcd750971ee7c66bfc75972ef7e13d7458
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-06-12 13:39:36 +0200

    Time: 83 ms (41.92%), Space: 42.1 MB (43.31%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -3,9 +3,12 @@
  * @return {number}
  */
 var average = function(salary) {
+    
     const tempSalary = [...salary];
     tempSalary.sort((a, b) => a-b);
+    
     let sumOfSalaries = 0;
+    
     for (let i = 1; i < tempSalary.length-1; i++) {
         sumOfSalaries += tempSalary[i];
     }
