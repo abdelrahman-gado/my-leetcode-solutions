e196d78f67b31a1212cd303c1454ba576daee79d
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-06-25 19:33:34 +0200

    Time: 138 ms (20.39%), Space: 43.6 MB (51.63%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -13,24 +13,15 @@ var isIsomorphic = function(s, t) {
     const map2 = new Map();
     
     for (let i = 0; i < s.length; i++) {
-        if (!map1.has(s[i]))  {
+        
+        if (!map1.has(s[i]) && !map2.has(t[i])) {
             map1.set(s[i], t[i]);
+            map2.set(t[i], s[i]);
+        } else if (!(map1.get(s[i]) === t[i] && map2.get(t[i]) === s[i])) {
+            return false;
         }
         
-        if (!map2.has(t[i])) {
-            map2.set(t[i], s[i]);
-        }
     }
     
-    
-    let created_t = "";
-    let created_s = "";
-    
-    for (let i = 0; i < t.length; i++) {
-        created_t += map1.get(s[i]);
-        created_s += map2.get(t[i]);
-    }
-    
-    
-    return created_t === t && created_s === s;
+    return true;
 };
