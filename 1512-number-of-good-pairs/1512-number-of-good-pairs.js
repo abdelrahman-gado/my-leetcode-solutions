2cd965ecd86ba3d18e4d55c2ca258fdd91006a58
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-08-18 08:25:28 +0200

    Time: 99 ms (32.59%), Space: 42.1 MB (47.54%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var numIdenticalPairs = function(nums) {
+    
+    let goodPairsCount = 0;
+    
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (i < j && nums[i] === nums[j]) {
+                goodPairsCount++;   
+            }
+        }
+    }
+    
+    return goodPairsCount;
+};
