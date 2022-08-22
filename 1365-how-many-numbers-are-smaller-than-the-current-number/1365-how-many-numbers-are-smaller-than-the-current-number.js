da7ce4bc29d2207e50b0693e0a6d4db1e8be1034
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-08-22 11:32:44 +0200

    Time: 137 ms (29.45%), Space: 44.2 MB (64.96%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -4,18 +4,13 @@
  */
 var smallerNumbersThanCurrent = function(nums) {
     
-    const smallerArr = [];
+    const sortedArr = [...nums];
+    sortedArr.sort((a, b) => a-b);
+    const finalArr = [];
+    
     for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-        for (let j = 0; j < nums.length; j++) {
-            if (j !== i && nums[j] < nums[i]) {
-                count++;
-            }
-        }
-        
-        smallerArr.push(count);
+        finalArr.push(sortedArr.indexOf(nums[i]));
     }
     
-    
-    return smallerArr;
+    return finalArr;
 };
