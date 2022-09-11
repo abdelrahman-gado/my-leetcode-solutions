819561a91168d06efc42e54b8d49446cc4e7a26c
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-09-11 17:14:52 +0200

    Time: 212 ms (12.23%), Space: 49.8 MB (26.30%) - LeetHub

---
+++ 654-maximum-binary-tree/654-maximum-binary-tree.js
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+
+
+
+var constructMaximumBinaryTree = function(nums) {
+    var buildTree = function (nums, start, end) {
+    
+        if (start > end) {
+            return null;
+        }
+
+        const m = Math.max(...(nums.slice(start, end+1)));
+        const indexOfm = nums.indexOf(m);
+
+        const root = new TreeNode(m);
+        root.left = buildTree(nums, start, indexOfm-1);
+        root.right = buildTree(nums, indexOfm+1, end);
+
+        return root;
+    }
+    
+    return buildTree(nums, 0, nums.length-1);
+};
