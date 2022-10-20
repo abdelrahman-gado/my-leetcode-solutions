aa471a44752b6b6b40e8db1d424017c2c852d8b0
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2022-10-20 16:40:32 +0200

    Time: 21 ms (93.75%), Space: 19.4 MB (31.25%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.php
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.php
@@ -8,12 +8,16 @@ class Solution {
     function arithmeticTriplets($nums, $diff) {
         $count = 0;
         for ($i = 0; $i < count($nums); $i++) {
+            $second = null;
             for ($j = $i + 1; $j < count($nums); $j++) {
-                for ($k = $j + 1; $k < count($nums); $k++) {
-                    if ($nums[$j] - $nums[$i] === $diff && $nums[$k] - $nums[$j] === $diff) {
-                        $count++;
-                    }
-                }
+                if ($second === null && $nums[$j] - $nums[$i] === $diff) {
+                    $second = $nums[$j];
+                    continue;
+                } 
+                
+                if (isset($second) && $second !== null && $nums[$j] - $second === $diff) {
+                    $count++;
+                }                
             }
         }
         
