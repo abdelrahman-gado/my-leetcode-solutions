6dca40bb35497c6f53ea717dfb8737c7b0aee05b
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2023-02-11 08:45:39 +0200

    Time: 71 ms (33.33%), Space: 20.8 MB (50.00%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.php
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.php
@@ -5,28 +5,8 @@ class Solution {
      * @return Integer
      */
     function maxProductDifference($nums) {
-        
-        $max = -1;
-        $max2 = -1;
-        $min = PHP_INT_MAX;
-        $min2 = PHP_INT_MAX;
-        
-        foreach ($nums as $i) {
-            
-            if ($i > $max) {          // 1
-                $max2 = $max;
-                $max = $i;
-            } else if ($i > $max2) {  // 2
-                $max2 = $i;
-            }
-            if ($i < $min) {          // 3
-                $min2 = $min;
-                $min = $i;
-            } else if ($i < $min2) {  // 4
-                $min2 = $i;
-            }
-        }
-        
-        return ($max * $max2) - ($min * $min2);
+        $len = count($nums);
+        sort($nums);
+        return ($nums[$len-1] * $nums[$len-2]) - ($nums[0] * $nums[1]); 
     }
 }
