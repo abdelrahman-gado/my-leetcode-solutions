b83d2f0f5adb4c5cbc3054e9965e16a0309d385a
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2023-02-11 10:28:17 +0200

    Time: 17 ms (51.58%), Space: 19.2 MB (90.50%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.php
+++ 1470-shuffle-the-array/1470-shuffle-the-array.php
@@ -6,11 +6,13 @@ class Solution {
      * @return Integer[]
      */
     function shuffle($nums, $n) {
+        
         $shuffledArr = [];
         for ($i = 0; $i < $n; $i++) {
             $shuffledArr[] = $nums[$i];
             $shuffledArr[] = $nums[$i + $n];
         }
+        
         
         return $shuffledArr;
     }
