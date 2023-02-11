193dde4c64adf8bdb5f518a57450cbeb5cda4f8a
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2023-02-11 09:25:26 +0200

    Time: 10 ms (44.44%), Space: 19.4 MB (44.44%) - LeetHub

--- 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.php
+++ 1360-number-of-days-between-two-dates/1360-number-of-days-between-two-dates.php
@@ -5,10 +5,10 @@ class Solution {
      * @param String $date2
      * @return Integer
      */
-    function daysBetweenDates($date1, $date2) {
-       
+    
+    function daysBetweenDates($date1, $date2) {        
         $d1 = new DateTime($date1);
         $d2 = new DateTime($date2);
-        return $d2->diff($d1)->format("%a");
+        return $d1->diff($d2)->format("%a");
     }
 }
