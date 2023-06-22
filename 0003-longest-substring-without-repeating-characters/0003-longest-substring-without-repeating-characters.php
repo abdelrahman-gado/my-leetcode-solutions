9d5ec8c9e6f54acae5481ab9aa837d1182bb9408
Abdelrahman Gado <88284519+abdelrahman-gado@users.noreply.github.com>
2023-06-22 14:23:17 +0300

    Time: 23 ms (63.03%), Space: 18.9 MB (93.53%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.php
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.php
@@ -7,29 +7,20 @@ class Solution {
     function lengthOfLongestSubstring($s) {
         
         $longestSubstringLength = 0;
+        $leftPointer = 0;
         $set = [];
         
         $sLength = strlen($s);
         for ($i = 0; $i < $sLength; $i++) {
-            if (!in_array($s[$i], $set)) {
-                $set[] = $s[$i];
-            } else {
-                if (count($set) > $longestSubstringLength) {
-                    $longestSubstringLength = count($set);
-                }
-                
-                $set[] = $s[$i];
-                $j = 0;
-                while ($set[$j] !== $s[$i]) {
-                    array_shift($set);
-                }
-                array_shift($set);
+            while (array_key_exists($s[$i], $set)) {
+                unset($set[$s[$leftPointer]]);
+                $leftPointer++;
             }
+            
+            $set[$s[$i]] = 1;
+            $longestSubstringLength = max($longestSubstringLength, $i - $leftPointer + 1);
         }
         
-        if (count($set) > $longestSubstringLength) {
-            $longestSubstringLength = count($set);
-        }
         
         return $longestSubstringLength;
         
